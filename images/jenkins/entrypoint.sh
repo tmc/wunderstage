4d31395909266ebc26cd31c1076b18d4a0c1a075
Travis Cline <travis.cline@gmail.com>
2016-08-18 16:17:30 -0700

    fix known_hosts population

--- images/jenkins/entrypoint.sh
+++ images/jenkins/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-mkdir /root/.ssh || echo ok
-ssh-keyscan -p 2222 -H ${DEIS_BUILDER} >> ~/.ssh/known_hosts || echo 'issue adding'
+mkdir ~jenkins/.ssh || echo
+touch ~jenkins/.ssh/known_hosts
+chown -R jenkins:jenkins ~jenkins/.ssh
+ssh-keyscan -p 2222 -H ${DEIS_BUILDER} >> ~jenkins/.ssh/known_hosts || echo 'issue adding'
 2>&1
 exec /usr/local/bin/jenkins.sh
